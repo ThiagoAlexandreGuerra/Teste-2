8859b554bf8a4ea1e2512d10c9e90fbe41061172
Thiago Alexandre Aleixo Pereira <15747523678@estudante.ufjf.br>
2024-05-30 09:52:16 -0300

    jn,m,

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 console. log("sxript")
 var names = ["marcos", "teodoro", "vinicios", "gabriel"]
 console. log (names)
+const managerlist = {
+casa: "benfica",
+nome: "thiago alexandre guerra"
+cidade: "belem do para"
+movies: [
+    the godfather
+    fast and furious
+    freedom django
+]
+}
+const findname = names. find( names => names ==="teodoro")
+console. log(findname)
